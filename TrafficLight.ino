0f6ce24e6fd73c223a947e158826da03237e2ba2
Rodrigo Lion Vizgarra <150470246+RLVizgarra@users.noreply.github.com>
2025-11-27 10:26:37 -0300

    Update TrafficLight.ino

--- TrafficLight.ino
+++ TrafficLight.ino
@@ -42,11 +42,11 @@ void loop(){
   digitalWrite(ledYellowOne, LOW);
   digitalWrite(ledRedTwo, LOW);
   digitalWrite(ledGreenTwo, HIGH);
-  delay(5000);
+  delay(5000); // Wait 5 seconds
 
   Serial.println("Phase 2 -> Yellow2 high.");
   digitalWrite(ledYellowTwo, HIGH);
-  delay(2000);
+  delay(2000); // Wait 2 seconds
 
   Serial.println("Phase 3 -> Red2 high. Green1 high.");
   digitalWrite(ledRedTwo, HIGH);
@@ -54,9 +54,9 @@ void loop(){
   digitalWrite(ledYellowTwo, LOW);
   digitalWrite(ledRedOne, LOW);
   digitalWrite(ledGreenOne, HIGH);
-  delay(5000);
+  delay(5000); // Wait 5 seconds
 
   Serial.println("Phase 4 -> Yellow1 high.");
   digitalWrite(ledYellowOne, HIGH);
-  delay(2000);
+  delay(2000); // Wait 2 seconds
 }
